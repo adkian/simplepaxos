43c50ceb212b597f863baf593812f0b0d433a481
Aditya Naik <naik@psu.edu>
2017-12-01 16:33:50 -0500

    started work on preliminary protocol

---
+++ prelim.py
@@ -0,0 +1,20 @@
+"""
+priest ledger format
+-------------------
+front #denotes the start of the actual ledger 
+0/1,n,0/1  #voted or not, ballot number, promised or not 
+promised or not explanation:       whether or not this priest has promised to not 
+                                   respond to maxvote requests after this ballot
+
+"""
+
+
+"""
+messenger relays messages to and from the leader and the priest
+"""
+class messenger:
+    
+class priest:
+
+class leader:
+    
